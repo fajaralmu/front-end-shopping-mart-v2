7833dc325b9e15cf311a6af7f0909a38d9f8467d
Fajar AM <somabangsa@gmail.com>
2021-01-14 05:38:36 +0700

    update font color

--- src/component/pages/home/HomeMain.tsx
+++ src/component/pages/home/HomeMain.tsx
@@ -25,13 +25,14 @@ class HomeMain extends BaseComponent {
                     style={{
                         marginTop: '20px',
                         backgroundImage: 'url("' + imageUrl + '")',
-                        backgroundSize: 'cover'
+                        backgroundSize: 'cover',
+                        color: applicationProfile.fontColor
                     }}
                 >
                     <h1 className="display-4">{applicationProfile.name}</h1>
-                    <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                    <p className="lead">{applicationProfile.welcomingMessage}</p>
                     <hr className="my-4" />
-                    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                    <p>{applicationProfile.shortDescription}</p>
                     <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
                 </div>
             </div>
