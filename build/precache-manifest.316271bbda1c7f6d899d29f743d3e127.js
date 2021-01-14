a106a313ec9adee5a1dd553773b4b09382be7d45
Fajar AM <somabangsa@gmail.com>
2021-01-14 21:41:43 +0700

    add build

---
+++ build/precache-manifest.316271bbda1c7f6d899d29f743d3e127.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32188a16227c137a7dcd740d76eaa2c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "638807038d63213d9ec7",
+    "url": "/static/css/main.8a47926e.chunk.css"
+  },
+  {
+    "revision": "9865f3a8e70c93dab1f4",
+    "url": "/static/js/2.860e293d.chunk.js"
+  },
+  {
+    "revision": "d162c07e3a32d536204fa997b073c340",
+    "url": "/static/js/2.860e293d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "638807038d63213d9ec7",
+    "url": "/static/js/main.b548e84b.chunk.js"
+  },
+  {
+    "revision": "87cf1de95aefd675cb05",
+    "url": "/static/js/runtime-main.04c66a24.js"
+  }
+]);
